0cc20bd734bfed9cc38275cf0c7955d6bd8c66c5
mauryappstudio <maury.app.studio@gmail.com>
2026-06-16 12:23:56 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'rifafacil-v1';
+const ASSETS = [
+  './rifa-finalv3.html',
+  './icon-rifa.png',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Fredoka+One&family=Nunito:wght@400;600;700;800;900&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./rifa-finalv3.html')))
+  );
+});
