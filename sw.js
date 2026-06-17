4566b3ed49d35d7b63d90186e9d9e0bc841f5afb
mauryappstudio <maury.app.studio@gmail.com>
2026-06-16 22:03:35 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,24 @@
-const CACHE = 'rifafacil-v2';
-const ASSETS = [
-  './index.html',
-  './icon-rifa.png',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Fredoka+One&family=Nunito:wght@400;600;700;800;900&display=swap'
-];
+const CACHE = 'rifafacil-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
+  // Siempre ir a la red primero, caché como respaldo
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    fetch(e.request).catch(() =>
+      caches.match(e.request).then(cached =>
+        cached || caches.match('./index.html')
+      )
+    )
   );
 });
