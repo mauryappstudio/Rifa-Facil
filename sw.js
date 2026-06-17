3ca8ec1683fb351eedeba4f42dc84f3684a2dd4f
mauryappstudio <maury.app.studio@gmail.com>
2026-06-16 21:53:32 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'rifafacil-v1';
+const CACHE = 'rifafacil-v2';
 const ASSETS = [
-  './rifa-finalv3.html',
+  './index.html',
   './icon-rifa.png',
   './manifest.json',
   'https://fonts.googleapis.com/css2?family=Fredoka+One&family=Nunito:wght@400;600;700;800;900&display=swap'
@@ -22,6 +22,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./rifa-finalv3.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
